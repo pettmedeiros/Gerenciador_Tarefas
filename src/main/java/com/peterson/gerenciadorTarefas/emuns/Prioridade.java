845a0fb1a3e964e7efc5734fc5a9ab5c7191007d
Peterson <pettm20@gmail.com>
2025-01-24 15:52:14 -0300

    Projeto Criado

---
+++ src/main/java/com/peterson/gerenciadorTarefas/emuns/Prioridade.java
@@ -0,0 +1,10 @@
+package com.peterson.gerenciadorTarefas.emuns;
+
+public enum Prioridade {
+	
+	BAIXA,
+	MEDIA,
+	ALTA,
+	URGENTE
+
+}
---
+++ src/main/java/com/peterson/gerenciadorTarefas/emuns/TaskStatus.java
@@ -0,0 +1,9 @@
+package com.peterson.gerenciadorTarefas.emuns;
+
+public enum TaskStatus {
+	
+	PENDENTE,
+	EM_PROGRESSO,
+	CONCLUIDA
+
+}
